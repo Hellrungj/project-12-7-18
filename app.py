5bde06f27bfc418b606c654c888aca887a72b2dc
Hellrungj <johnjameshellrung@gmail.com>
2018-12-07 15:46:51 -0600

    Make calu form in the index page

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    result = ["", ""]
+    if request.method == 'POST':
+        result[0] = request.form["dil"]
+        result[1] = request.form["vol"]
+    return render_template('index.html', result=result)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
